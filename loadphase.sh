881970f8a60e5f6fe28bb3ae85150b94fe2706eb
Tamires Brito da Silva <tamiresbritodasilva@gmail.com>
2017-03-11 17:01:28 -0800

    Update loadphase.sh

--- loadphase.sh
+++ loadphase.sh
@@ -1,5 +1,23 @@
+#!/bin/bash
+
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
+
 echo "------------------------------------------------------------------------------
 				Loading Data
-------------------------------------------------------------------------------"
+------------------------------------------------------------------------------
+
+Is linkbench-master folder on the current directory? [Y/n] "
+read answer
+if [ "$answer" == "N" ] || [ "$answer" == "n" ]; then
+	echo "Type the path to the directory where linkbench-master folder is: "
+	read path
+	path=$path/linkbench-master
+else
+	path=linkbench-master
+fi
+
+cd $path
 
 ./bin/linkbench -c config/MyConfig.properties -csvstats load-phase-final-stats.csv -csvstream load-phase-streaming-stats.csv -L load-phase.log -l
+
+cd $MY_PATH
